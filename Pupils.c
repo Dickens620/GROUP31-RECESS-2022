68906fcba599f6d5ee5a130aadee042e7a1fe7b1
Gabula John Stephen <77186258+Gabula145@users.noreply.github.com>
2022-02-02 03:28:14 +0300

    Update Pupils.c

--- Pupils.c
+++ Pupils.c
@@ -1,39 +1,162 @@
-
+#include <stdio.h>
+#include <mysql.h>
 #include <stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<time.h>
-int main()
+#include <string.h>
+MYSQL *getConnection();
+void requestActivation(char *userId, MYSQL *conn);
+char *commandPrompt();
+void commandInterpreter(char *);
+void main()
 {
-     printf("\t\tMENU\n1.VIEWALL\n2.CHECKSTATUS\n3.VIEWASSIGNMENT\n4.CHECKDATES\n5.REQUESTACTIVATION\n");
-    char choice[20];
-     char a[20]="VIEWALL";
-     char b[20]="CHECKSTATUS";
-    char c[20]="VIEWASSIGNMENT";
-    char d[20]="CHECKDATES";
-    char e[20]="REQUESTACTIVATION";
-    printf("ENTER CHOICE FROM MENU:");
-    scanf("%s",choice);
-    if(strcmp(a,choice)==0){
-        printf("\n |ASSIGNMENT_ID\t|DATE\t|ATTEMPTED");
-    }
-    else if(strcmp(b,choice)==0){
-        printf("\n |ASSIGNMENT_ID\t|ATTEMPTED\t|AVERAGE_SCORE\t|PERCENTAGE_MISSED\t|PERCENTAGE_ATTEMPTED");
-    }
-    else if(strcmp(c,choice)==0){
-        printf("\n |ASSIGNMENT_ID\t|DURATION");
-    }
-    else if(strcmp(d,choice)==0){
-        printf("\n |ASSIGNMENT_ID\t|VALID DATE");
-    }
-    else if(strcmp(e,choice)==0){
-        printf("\n ACTIVATION REQUEST SENT TO TEACHER");
-    }
-    else{
-            //INCASE OF INPUT THAT IS NOT ON THE MENU,IT RETURNS THE MENU
-        main();
-    }
+    // Mysql variables
+    MYSQL *con;
+    MYSQL_RES *result;
+    MYSQL_ROW row;
 
-    return 0;
+    // user info variables for session tracking
+    char userId[50];
+    char firstName[50];
+    char lastName[50];
+    char userCode[20];
+    char status[50];
+    char phoneNumber[30];
+    int loop = 1;
+    while (loop)
+    {
+        //
+        // Get user information
+        printf("\t Welcome Back!\t\n");
+        printf("\t Enter Phone Number: ");
+        scanf("%s", phoneNumber);
+        printf("\n\t Enter Usercode: ");
+        scanf("%s", userCode);
+
+        // Connect to the database
+        con = mysql_init(NULL);
+        con = getConnection();
+
+        // Login logic
+        char query[255];
+
+        sprintf(query, "SELECT * FROM pupils WHERE user_code='%s' AND phone_no='%s' ", userCode, phoneNumber);
+
+        // Run query and see, if it returns a non zero value, it means failure
+        if (mysql_query(con, query) != 0)
+        {
+            printf("Query failed  with the following message:\n");
+            printf("%s\n", mysql_error(con));
+            exit(1);
+        }
+
+        // Query successful now check if the user exists
+        result = mysql_store_result(con);
+
+        int total = (int)mysql_num_rows(result);
+        if (total == 0)
+        {
+            printf("Wrong Usernme or Posscode\n");
+        }
+        else
+        {
+            printf("You have Logged in successfully\n");
+
+            while ((row = mysql_fetch_row(result)) != NULL)
+            {
+                strcpy(userId, (char *)row[0]);
+                strcpy(firstName, (char *)row[1]);
+                strcpy(lastName, (char *)row[2]);
+                strcpy(status, (char *)row[7]);
+            }
+            system("clear");
+            puts("Welcome Back, ");
+            printf("\n\tFull Name: %s %s\t Status: %s\n", firstName, lastName, status);
+            // requestActivation(userId, con);
+            // printf("\n\n\n\n");
+            char command[255];
+        cmd:
+            printf("Enter Command _> ");
+            fflush(stdin);
+
+            scanf("%s", command);
+            // Process command
+            if (strcmp("Viewall", command) == 0)
+            {
+            }
+            else if (strcmp("Checkstatus", command) == 0)
+            {
+                printf("\nStatus: %s\n\n\n", status);
+                goto cmd;
+            }
+            else if (strcmp("Viewassignment", command) == 0)
+            {
+            }
+            else if (strcmp("Checkdates", command) == 0)
+            {
+            }
+            else if (strcmp("RequestActivation", command) == 0)
+            {
+                printf("\nRequesting Activation...\n");
+                requestActivation(userId, con);
+                printf("\nActivation Request Sent!\n");
+                goto cmd;
+            }
+            else if (strcmp("Logout", command) == 0)
+            {
+                printf("Logged out!\n\n");
+                exit(0);
+            }
+            else
+            {
+                printf("\nSorry Command %s is unrecognized\n", command);
+                goto cmd;
+            }
+        }
+    }
+    printf("Bye!!");
+    // printf("%d",howMany);
+}
+
+void requestActivation(char *userId, MYSQL *conn)
+{
+
+    char query[255];
+    sprintf(query, "INSERT INTO activation_requests SET pupil_id='%s'", userId);
+    mysql_query(conn, query);
+}
+MYSQL *getConnection()
+{
+    MYSQL *con;
+    con = mysql_init(NULL);
+
+    if (mysql_real_connect(con, "127.0.0.1", "root", "", "kcpms", 3306, NULL, 0) == NULL)
+    {
+        printf("The authentication failed with the following message:\n");
+        printf("%s\n", mysql_error(con));
+        exit(1);
+    }
+    return con;
+}
+
+void commandInterpreter(char *command)
+{
+
+    if (strcmp("Viewall", command) == 0)
+    {
+    }
+    else if (strcmp("Checkstatus", command) == 0)
+    {
+    }
+    else if (strcmp("Viewassignment", command) == 0)
+    {
+    }
+    else if (strcmp("Checkdates", command) == 0)
+    {
+    }
+    else if (strcmp("RequestActivation", command) == 0)
+    {
+    }
+    else
+    {
+    }
 }
 
